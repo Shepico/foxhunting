57aa8048fe07cf19430a51ae0977e47fc3edab6b
Shepico <Shepico@gmail.com>
2018-10-15 11:07:42 +0400

    Добавлено пустое поле, в ресурсы Создана матрица

---
+++ src/MatrixGame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class MatrixGame {
+    private int size_map;
+    private JTable matrix;
+
+    MatrixGame (int sizeField, boolean systemMatrix) {
+        size_map = sizeField;
+        createMatrixGame();
+        if (systemMatrix){
+            fillMatrix();
+        }
+
+    }
+///////public/////////////////
+    public JTable getMatrix() {
+        return matrix;
+    }
+///////private////////////////
+    private void createMatrixGame (){
+        matrix = new JTable(size_map,size_map);
+    }
+
+    private void fillMatrix(){
+
+    }
+}
